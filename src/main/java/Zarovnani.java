5eb87ae3402efcba94f860052496897187671741
Honza Kočka <kockahonza@gmail.com>
2018-05-20 22:31:32 +0200

    first commit

---
+++ src/main/java/Zarovnani.java
@@ -0,0 +1,5 @@
+public class Zarovnani {
+	public static void main(String[] args) {
+		System.out.println("Zarovnani here");
+	}
+}
---
+++ src/test/java/ZarovnaniTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ZarovnaniTest {
+	@Test
+	public void ZarovnaniExists() {
+		Zarovnani SMC = new Zarovnani();
+	}
+}
